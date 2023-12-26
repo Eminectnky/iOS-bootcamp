1b4a1ada4d4b437c1efbd917205b0d220f9dc738
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2023-12-26 12:54:42 +0300

    Date and time plugins

--- Date&TimePickerKullanimi/Date&TimePickerKullanimi/ViewController.swift
+++ Date&TimePickerKullanimi/Date&TimePickerKullanimi/ViewController.swift
@@ -26,14 +26,38 @@ class ViewController: UIViewController {
         timePicker = UIDatePicker()
         timePicker?.datePickerMode = .time
         tfSaat.inputView = timePicker
+        
+        if #available(iOS 13.4, *){
+            datePicker?.preferredDatePickerStyle = .wheels
+            timePicker?.preferredDatePickerStyle = .wheels
+        }
        
         let dokunmaAlgilama = UITapGestureRecognizer(target: self, action: #selector(dokunmaAlgilamaMetod))
         view.addGestureRecognizer(dokunmaAlgilama) //bu yapı ekrana dokunulduğu zaman dokunmaAlgilamaMetod unu çalıştırır
+        
+       datePicker?.addTarget(self, action: #selector(tarihGoster(uiDatePicker:)), for: .valueChanged)
+        
+       timePicker?.addTarget(self, action: #selector(saatGoster(uiDatePicker:)), for: .valueChanged)
     }
     @objc func dokunmaAlgilamaMetod(){
         print("Ekrana dokunuldu")
         view.endEditing(true) //sayfa üzerinde açılmış yapıları kapatmak için kullanılır 
     }
-
+    
+    
+    @objc func tarihGoster(uiDatePicker: UIDatePicker){
+        let format = DateFormatter()
+        format.dateFormat = "MM/dd/yyyy"
+        let alinanTarih = format.string(from: uiDatePicker.date)
+        tfTarih.text = alinanTarih
+    }
+    
+    
+    @objc func saatGoster(uiDatePicker: UIDatePicker){
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm"
+        let alinanSaat = format.string(from: uiDatePicker.date)
+        tfSaat.text = alinanSaat
+    }
 }
 
