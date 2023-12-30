a0c59d8c0c574f658e84841150cc50b527606002
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2023-12-30 16:25:33 +0300

    TabBar created

---
+++ Odev6/Odev6/KategorilerVC.swift
@@ -0,0 +1,18 @@
+//
+//  KategorilerVC.swift
+//  Odev6
+//
+//  Created by Emine CETINKAYA on 30.12.2023.
+//
+
+import UIKit
+
+class KategorilerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
